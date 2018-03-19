e53eb50d2c8336dc7e051207215ebb395fa91074
Zac Webb <me@zacwebb.com>
2018-03-20 09:38:54 +1300

    Added buttons to Dashboard context menu

--- canvas.js
+++ canvas.js
@@ -1,3 +1,22 @@
 $(function() {
-    $('body').append('<p>gay</p>')
+    $('.icon-more').click(function(){
+        setTimeout(showButton, 100);
+    });
+
+    function showButton() {
+        $('.ColorPicker__Container').append('\
+            <div class="changeImage _2xB3_kf">Image</div>\
+            <div class="imageButton _922amOO _16-3B-L AIqd8V_ remove">Remove Image</div>\
+            <div class="imageButton _922amOO _16-3B-L AIqd8V_ original">Set to Original</div>\
+            <div class="imageButton _922amOO _16-3B-L AIqd8V_ custom">Custom Image</div>\
+        ');
+
+        $('._2dovB3N').css({
+            "height":400,
+        });
+
+        $('.imageButton.remove').click(function(){
+            alert('re')
+        });
+    }
 });
