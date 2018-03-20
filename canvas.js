33b85e6d0cc48affb8edaf479eaf33004ad52ec7
Zac Webb <me@zacwebb.com>
2018-03-20 14:49:21 +1300

    Menu buttons now function correctly

--- canvas.js
+++ canvas.js
@@ -1,9 +1,23 @@
 $(function() {
-    $('.icon-more').click(function(){
-        setTimeout(showButton, 100);
+    var cardNumber = 0;
+
+    // Gives each Dashboard card a unique identifier
+    $('.ic-DashboardCard').each(function(){
+        $(this).addClass('imageCard' + cardNumber)
+        cardNumber++;
     });
 
-    function showButton() {
+    $('.icon-more').click(function(){
+        parentCard = $(this).parents()[4];
+        parentCard = $(parentCard).attr("class").split(' ')[1];
+
+        setTimeout(function(){
+            showMenu(parentCard)
+        }, 100);
+    });
+
+    function showMenu(parentCard) {
+        // Add Image buttons to menu
         $('.ColorPicker__Container').append('\
             <div class="changeImage _2xB3_kf">Image</div>\
             <div class="imageButton _922amOO _16-3B-L AIqd8V_ remove">Remove Image</div>\
@@ -11,12 +25,56 @@ $(function() {
             <div class="imageButton _922amOO _16-3B-L AIqd8V_ custom">Custom Image</div>\
         ');
 
+        // Set height of menu
         $('._2dovB3N').css({
             "height":400,
         });
 
+        $('.imageButton.original').click(function(){
+            chrome.storage.sync.get(['removedList'], function(result) {
+                var i = result.removedList.indexOf(parentCard);
+                if (i >= 0) {
+                    var value = result.removedList
+                    value.splice(i ,1);
+                    chrome.storage.sync.set({removedList: value});
+                }
+            });
+        });
+
         $('.imageButton.remove').click(function(){
-            alert('re')
+            chrome.storage.sync.get(['removedList'], function(result) {
+                if (result.removedList == '') {
+                    value = [parentCard];
+                    chrome.storage.sync.set({removedList: value});
+
+                } else {
+                    if (result.removedList.indexOf(parentCard) == -1) {
+                        var value = result.removedList
+                        value.push(parentCard)
+                        chrome.storage.sync.set({removedList: value});
+                    }
+                }
+            });
+        });
+
+        $('.imageButton.custom').click(function(){
+            chrome.storage.sync.set({removedList: []}, function() {
+                console.warn('List is wiped');
+            });
+        });
+
+        $('.imageButton').click(function(){
+            refreshImages();
         });
     }
+
+    function refreshImages() {
+        chrome.storage.sync.get(['removedList'], function(result) {
+            result.removedList.forEach(function(item) {
+                $('.' + item + ' .ic-DashboardCard__header_image').addClass('noImage');
+            });
+        });
+    }
+
+    refreshImages();
 });
